6d5ad66a40c621109c6f0151b8f64d3e9bfffc11
Dnyaneshwar Dewadkar <ddewadka@in.ibm.com>
2015-10-15 15:22:42 +0530

    Added creating a new DB on local.

--- src/main/java/com/ikl/lab/main/MainDemo.java
+++ src/main/java/com/ikl/lab/main/MainDemo.java
@@ -9,6 +9,7 @@ import org.apache.commons.io.FileUtils;
 import com.ikl.lab.beans.Concept;
 import com.ikl.lab.neo4j.AlchemiResponseProcessor;
 import com.ikl.lab.neo4j.Neo4jConceptWriter;
+import com.ikl.lab.preprocessing.pdfbox.ExtractText;
 import com.ikl.lab.preprocessing.pdfbox.PDFTextExtractor;
 import com.ikl.lab.util.IklFileUtil;
 
@@ -21,6 +22,9 @@ public class MainDemo {
 	public static void main(String[] args) {
 
 		MainDemo demo = new MainDemo();
+
+		// ---------------------------- to run on single file - on command
+		// prompt : Just uncomment this block and comment other blocks
 		// if (args.length < 2) {
 		// System.out
 		// .println(" Please specify two arguments : dir path for index and PDF file path.");
@@ -28,7 +32,17 @@ public class MainDemo {
 		// }
 		//
 		// demo.initSingleFileFlow(args[0], args[1]);
+
+		// ------------------------- To test on local environment in IDE use
+		// this block
 		demo.init();
+
+		// ---------------------------- If need to run in IDE for multiple files
+		// soterd in folder use this block.
+		// final String dbStorePath = "<PATH TO STORE DB CREATED BY PROGRAM>";
+		// final String dirPath =
+		// "<WHERE ALL PDF FILES STORED AND NEED TO USE IT FOR TEST>";
+		// demo.initFolderFlow(dbStorePath, dirPath);
 	}
 
 	public void init() {
@@ -64,4 +78,27 @@ public class MainDemo {
 		}
 
 	}
+
+	public void initFolderFlow(final String dbStorePath, final String dirPath) {
+
+		ExtractText et = new ExtractText();
+
+		List<File> files = et.listFile(dirPath);
+
+		for (File file : files) {
+
+			if (file.isFile()) {
+				String content = PDFTextExtractor.pdftoText(file
+						.getAbsolutePath());
+				AlchemiResponseProcessor rr = new AlchemiResponseProcessor();
+				List<Concept> concepts = rr.getDocConcepts(content);
+				Neo4jConceptWriter ncw = new Neo4jConceptWriter(dbStorePath);
+				ncw.writeConceptToNeo4j(concepts, file.getName());
+			} else {
+				System.out.println("  Please check file path.");
+			}
+		}
+
+	}
+
 }
